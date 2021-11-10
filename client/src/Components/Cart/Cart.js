bf25d12bd5792f82c7f3fd725ea6022fda27a374
Paolobd <paolobodnarescul@gmail.com>
2021-11-10 21:55:57 +0100

    GUI Temporary Cart and Fixed imports

---
+++ client/src/Components/Cart/Cart.js
@@ -0,0 +1,75 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../../App.css';
+import { ListGroup, Col, Row, Image, Button, Modal } from "react-bootstrap";
+import mainLogo from "../../cipolle-dorate-bio.jpg";
+import cartIcon from "../../cart-icon.png";
+import { useState } from "react";
+
+function Cart() {
+    const [show, setShow] = useState(false);
+
+    const close = () => setShow(false);
+    const open = () => setShow(true);
+
+
+    return (<>
+        <Button className="showCartButton" onClick={open}>
+            <Image src={cartIcon} fluid />
+            10
+        </Button>
+
+        <Modal show={show} onHide={close} scrollable>
+            <Modal.Header closeButton style={{backgroundColor: "#6da8fd"}}>
+                <Col style={{ textAlign: "center" }}>
+                    <h3>
+                        <small>Your Cart (10) </small> <br />
+                        <b>Subtotal: € 2.00</b>
+                    </h3>
+                    <Button variant="success">Confirm Order</Button>
+                </Col>
+            </Modal.Header>
+            <Modal.Body>
+                <CartProduct />
+                <CartProduct />
+                <CartProduct />
+                <CartProduct />
+                <CartProduct />
+            </Modal.Body>
+        </Modal>
+    </>);
+}
+
+function CartProduct() {
+    return (
+        
+            <Row style={{borderBottom: "2px solid black", paddingTop: "5%", paddingBottom: "5%"}}>
+                <Button className="cartButtons delete" variant="danger">X</Button>
+                <Col>
+                    <Image src={mainLogo} fluid thumbnail/>
+                    <div style={{ textAlign: "center", fontWeight: "bold", fontSize: "25px" }}>Cipolla </div>
+                    <div style={{ textAlign: "center", fontSize: "15px" }}>Farmer: Mario Rossi</div>
+                </Col>
+                <Col style={{ textAlign: "center" }}>
+                    <br />
+                    <br />
+                    <div style={{ textAlign: "center", fontWeight: "bold", fontSize: "25px" }}>€ 0.10 &nbsp; </div>
+                    <br />
+                    <Row>
+                        <Col>
+                            <Button className="cartButtons" variant="primary">-</Button>
+                        </Col>
+                        <Col>
+                            <div style={{ textAlign: "center", fontWeight: "bold", fontSize: "25px" }}>2</div>
+                        </Col>
+                        <Col>
+                            <Button className="cartButtons" variant="primary">+</Button>
+                        </Col>
+                    </Row>
+                    <br />
+                    <div style={{ fontSize: "15px" }}>5 pieces available</div>
+                </Col>
+            </Row>
+    );
+}
+
+export default Cart;
--- client/src/Components/ProductList/ProductList.js
+++ client/src/Components/ProductList/ProductList.js
@@ -1,30 +1,29 @@
-import './ProductList.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {CardGroup, Col, Container, Row} from "react-bootstrap";
+import '../../App.css';
+import { CardGroup, Col, Container, Row, Button, Image } from "react-bootstrap";
 import Product from "../Product/Product";
 import SideBar from "../SideBar";
-import NavBar from "../../NavBar";
-import {useState} from "react";
+import Cart from "../Cart/Cart"
+import NavBar from "../NavBar";
+import { useState } from "react";
 
 function ProductList() {
-    const [products,setProducts]= useState([<Product/>,<Product/>,<Product/>,<Product/>,<Product/>,<Product/>,<Product/>,<Product/>,<Product/>,<Product/>]);
-
-
-
+    const [products, setProducts] = useState([<Product />, <Product />, <Product />, <Product />, <Product />, <Product />, <Product />, <Product />, <Product />, <Product />]);
 
     return (
-        <Row >
-        <Col xs={2} md={2}>
-        <SideBar></SideBar>
-         </Col>
-           <Col xs={10} md={10} className="main">
-            <Row xs={2} md={5} className="g-4">
+        <>
+            <Row>
+                <Col xs={2} md={2}  >
+                    <SideBar></SideBar>
+                </Col>
+                <Col xs={10} md={10} className="main">
+                    <Row xs={2} md={5} className="g-4">
                         {products}
+                    </Row>
+                </Col>
             </Row>
-           </Col>
-        </Row>
-
-
+            <Cart/>
+        </>
     );
 }
 
