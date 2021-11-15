e64636859f3702aaf1810476f917124968955441
Alireza Banouie <a_r_b_eng@hotmail.com>
2021-11-15 20:39:38 +0100

    pulire codice di Api

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@
 const express = require('express');
 const morgan = require('morgan'); // logging middleware
 const { check, validationResult } = require('express-validator'); // validation middleware
-// const PDao = require('./product-dao'); // module for accessing the exams in the DB
 const productDao = require('./product-dao');
 const orderDao = require('./order-dao');
 const Dao = require('./dao.js');
@@ -12,9 +11,9 @@ const passport = require('passport'); // auth middleware
 const LocalStrategy = require('passport-local').Strategy; // username and password for login
 const session = require('express-session'); // enable sessions
 
-/*** Set up Passport ***/
-// set up the "username and password" login strategy
-// by setting a function to verify username and password
+/* Set up Passport **
+set up the "username and password" login strategy
+by setting a function to verify username and password*/
 passport.use(new LocalStrategy(
     function (username, password, done) {
         userDao.getUser(username, password).then((user) => {
@@ -26,8 +25,8 @@ passport.use(new LocalStrategy(
     }
 ));
 
-// serialize and de-serialize the user (user object <-> session)
-// we serialize the user id and we store it in the session: the session is very small in this way
+/* serialize and de-serialize the user (user object <-> session)
+ we serialize the user id and we store it in the session: the session is very small in this way*/
 passport.serializeUser((user, done) => {
     done(null, user.id);
 });
@@ -70,177 +69,22 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-
-/*** APIs ***/
-
-// //GET /api/product
-// app.get('/api/products', async (req, res) => {
-//     PDao.getProducts()
-//         .then(products => res.json(products))
-//         .catch(() => res.status(500).end());
-// });
-
-app.post('/api/new_client', [
-    check(['name']).notEmpty(),
-    check('surname').notEmpty(),      
-    check('email').notEmpty(),
-    check('password').isLength({min:8, max:30}),
-    check('phoneNumber').isLength({ min: 10, max:10 }),
-    check('address').notEmpty(), 
-  ], async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(422).json({errors: errors.array()});
-    }
-  
-    const client = {
-        password: req.body.password,
-        name: req.body.name,
-        surname: req.body.surname,
-        email: req.body.email,
-        phoneNumber: req.body.phoneNumber,
-        accessType: req.body.accessType,
-        wallet: req.body.wallet,
-        address: req.body.address
-    };
-
-    try {
-        await Dao.createClient(client);
-        res.status(201).end();
-    } catch (err) {
-        res.status(503).json({ error: `Database error during the creation of client.` });
-    }
+// DELETE /sessions/current ->logout
+app.delete('/api/sessions/current', (req, res) => {
+    req.logout();
+    res.end();
 });
 
-//**** Api: Get All User For Admin ****//
-app.get('/api/clients', isLoggedIn, async (req, res) => {
-    try {
-        const result = await userDao.getUsersByAccessType(3);
-        if (result.error)
-            res.status(404).json(result);
-        else
-            res.json(result);
-    } catch (err) {
-        res.status(500).end();
+// GET /sessions/current-> check whether the user is logged in or not
+app.get('/api/sessions/current', (req, res) => {
+    if (req.isAuthenticated()) {
+        res.status(200).json(req.user);
     }
-    console.log(res);
+    else
+        res.status(401).json({ error: 'Unauthenticated user!' });
 });
 
-
-//**** Api: Get All Products For All Users By paging ****//
-app.get('/api/products', async (req, res) => {
-    try {
-        const result = await productDao.getProducts(req.query.page ? req.query.page : 0);
-        if (result.error)
-            res.status(404).json(result);
-        else {
-            res.json(result);
-        }
-    } catch (err) {
-        console.log(err)
-        res.status(500).end();
-    }
-});
-//**** Api: Get Details Of A Products For All Users ****//
-app.get('/api/products/:id', async (req, res) => {
-    try {
-        const result = await productDao.getProduct(req.params.id);
-        if (result.error)
-            res.status(404).json(result);
-        else
-            res.json(result);
-    } catch (err) {
-        res.status(500).end();
-    }
-});
-
-app.get('/api/types', async (req, res) => {
-    try {
-        const result = await productDao.getTypes();
-        if (result.error) {
-            res.status(404).json(result);
-        }
-        else {
-            res.json(result);
-        }
-    } catch (err) {
-        res.status(500).end();
-    }
-});
-
-app.get('/api/products/type/:typeId', async (req, res) => {
-    try {
-        const result = await productDao.getProductsByType(req.params.typeId);
-        if (result.error) {
-            res.status(404).json(result);
-        }
-        else {
-            res.json(result);
-        }
-    } catch (err) {
-        res.status(500).end();
-    }
-})
-
-// Order
-
-// ,isLoggedIn
-app.post('/api/booking', [
-    check('bookingStartDate').isDate({ format: 'YYYY-MM-DD', strictMode: true }),
-    check('totalPrice').isDecimal(),
-    check('state').isInt(),
-    check('pickupTime').isDate({ format: 'YYYY-MM-DD', strictMode: true })
-], async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(422).json({ errors: errors.array() });
-    }
-    const booking = {
-        bookingStartDate: req.body.bookingStartDate,
-        totalPrice: req.body.totalPrice,
-        state: req.body.state,
-        pickupTime: req.body.pickupTime,
-        deliveryTime: req.body.deliveryTIme,
-        userId: req.body.userId ? req.body.userId : req.user.id
-    };
-    console.log(booking)
-    var productsJson = req.body.products;
-    JSON.stringify(productsJson);
-    try {
-        if (productsJson.length > 0) {
-            var bookingId = await orderDao.createBooking(booking, req.body.userId);
-            productsJson.forEach(function (element, index) {
-                PostOrderProduct(element, bookingId);
-            });
-
-            return res.json(bookingId);
-        }
-        else {
-            return res.status(503).json({ error: `The minimum number of  product in booking is 1 .` });
-        }
-
-    } catch (err) {
-        return res.status(503).json({ error: `Database error during the creation of Booking.` });
-    }
-});
-
-
-async function PostOrderProduct(element, bookingId) {
-    console.log('calling');
-    const bookingProduct = {
-        bookingId: bookingId,
-        productId: element.productId,
-        quantity: element.quantity,
-        price: element.price,
-    };
-    var bookingProductIdId = await orderDao.createBookingAndProduct(bookingProduct);
-
-    // expected output: "resolved"
-}
-/*** Users APIs ***/
-
-// POST /sessions
-// login
+// POST /sessions -> login
 app.post('/api/sessions', function (req, res, next) {
     passport.authenticate('local', (err, user, info) => {
         if (err)
@@ -262,54 +106,189 @@ app.post('/api/sessions', function (req, res, next) {
 });
 
 
-
-app.put('/api/bookings/:id', [
-    check('state').isInt({min:0, max:2}),    
-  ], async (req, res) => {
+//****************************************************** */
+//                User API
+//****************************************************** */
+//**** Post New Client****//
+app.post('/api/new_client', [
+    check(['name']).notEmpty(),
+    check('surname').notEmpty(),
+    check('email').notEmpty(),
+    check('password').isLength({ min: 8, max: 30 }),
+    check('phoneNumber').isLength({ min: 10, max: 10 }),
+    check('address').notEmpty(),
+], async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(422).json({errors: errors.array()});
+        return res.status(422).json({ errors: errors.array() });
     }
-  
+
+    const client = {
+        password: req.body.password,
+        name: req.body.name,
+        surname: req.body.surname,
+        email: req.body.email,
+        phoneNumber: req.body.phoneNumber,
+        accessType: req.body.accessType,
+        wallet: req.body.wallet,
+        address: req.body.address
+    };
+
+    try {
+        await Dao.createClient(client);
+        res.status(201).end();
+    } catch (err) {
+        res.status(503).json({ error: `Database error during the creation of client.` });
+    }
+});
+
+
+//**** Get All User For Admin ****//
+app.get('/api/clients', isLoggedIn, async (req, res) => {
+    try {
+        const result = await userDao.getUsersByAccessType(3);
+        if (result.error)
+            res.status(404).json(result);
+        else
+            res.json(result);
+    } catch (err) {
+        res.status(500).end();
+    }
+    console.log(res);
+});
+
+//****************************************************** */
+//                Products API
+//****************************************************** */
+//Get All Products For All Users By paging
+app.get('/api/products', async (req, res) => {
+    try {
+        const result = await productDao.getProducts(req.query.page ? req.query.page : 0);
+        if (result.error)
+            res.status(404).json(result);
+        else {
+            res.json(result);
+        }
+    } catch (err) {
+        console.log(err)
+        res.status(500).end();
+    }
+});
+//**** Get A Product By Id ****//
+app.get('/api/products/:id', async (req, res) => {
+    try {
+        const result = await productDao.getProduct(req.params.id);
+        if (result.error)
+            res.status(404).json(result);
+        else
+            res.json(result);
+    } catch (err) {
+        res.status(500).end();
+    }
+});
+/*** Get All Product Type ***/
+app.get('/api/types', async (req, res) => {
+    try {
+        const result = await productDao.getTypes();
+        if (result.error) {
+            res.status(404).json(result);
+        }
+        else {
+            res.json(result);
+        }
+    } catch (err) {
+        res.status(500).end();
+    }
+});
+/*** Get Oroducts By TypeId ***/
+app.get('/api/products/type/:typeId', async (req, res) => {
+    try {
+        const result = await productDao.getProductsByType(req.params.typeId);
+        if (result.error) {
+            res.status(404).json(result);
+        }
+        else {
+            res.json(result);
+        }
+    } catch (err) {
+        res.status(500).end();
+    }
+})
+
+//****************************************************** */
+//                Booking API
+//****************************************************** */
+/*** Post Booking  ***/
+app.post('/api/booking', isLoggedIn, [
+    check('bookingStartDate').isDate({ format: 'YYYY-MM-DD', strictMode: true }),
+    check('totalPrice').isDecimal(),
+    check('state').isInt()
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
+    }
+    const booking = {
+        bookingStartDate: req.body.bookingStartDate,
+        totalPrice: req.body.totalPrice,
+        state: req.body.state,
+        userId: req.body.userId ? req.body.userId : req.user.id
+    };
+    var productsJson = req.body.products;
+    JSON.stringify(productsJson);
+    try {
+        if (productsJson.length > 0) {
+            var bookingId = await orderDao.createBooking(booking, req.body.userId);
+            productsJson.forEach(function (element, index) {
+                PostOrderProduct(element, bookingId);
+            });
+            return res.json({id:bookingId});
+        }
+        else {
+            return res.status(500).json({ error: `The minimum number of  product in booking is 1 .` });
+        }
+
+    } catch (err) {
+        return res.status(503).json({ error: `Database error during the creation of Booking.` });
+    }
+});
+
+/*** Booking Post Products ***/
+async function PostOrderProduct(element, bookingId) {
+    const bookingProduct = {
+        bookingId: bookingId,
+        productId: element.productId,
+        quantity: element.quantity,
+        price: element.price,
+    };
+    await orderDao.createBookingAndProduct(bookingProduct);
+    updateProductQUantity(element.quantity, element.productId);
+}
+/*** Update Product Quantity After Booking ***/
+async function updateProductQUantity(productId) {
+    await orderDao.updateProductQuantity(bookingProduct);
+}
+
+/*** Get Booking With ID ***/
+app.put('/api/bookings/:id', [
+    check('state').isInt({ min: 0, max: 2 }),
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
+    }
     const state = req.body.state;
-    
-  
     // you can also check here if the code passed in the URL matches with the code in req.body
     try {
-      await orderDao.updateBookingState(state, req.params.id);
-      //res.status(200).end();
-      setTimeout(()=> res.status(200).end(), 2000);
-    } catch(err) {
-      res.status(503).json({error: `Database error during the update of booking state ${req.params.id}.`});
+        await orderDao.updateBookingState(state, req.params.id);
+        //res.status(200).end();
+        setTimeout(() => res.status(200).end(), 2000);
+    } catch (err) {
+        res.status(503).json({ error: `Database error during the update of booking state ${req.params.id}.` });
     }
-  
-  });
 
-// ALTERNATIVE: if we are not interested in sending error messages...
-/*
-app.post('/api/sessions', passport.authenticate('local'), (req,res) => {
-  // If this function gets called, authentication was successful.
-  // `req.user` contains the authenticated user.
-  res.json(req.user);
-});
-*/
-
-// DELETE /sessions/current
-// logout
-app.delete('/api/sessions/current', (req, res) => {
-    req.logout();
-    res.end();
 });
 
-// GET /sessions/current
-// check whether the user is logged in or not
-app.get('/api/sessions/current', (req, res) => {
-    if (req.isAuthenticated()) {
-        res.status(200).json(req.user);
-    }
-    else
-        res.status(401).json({ error: 'Unauthenticated user!' });
-});
 
 
 // Activate the server
