38e3ee89e3c3d935274665a7a78cb6b111b52af9
Giovanni De Florio s292457@studenti.polito.it <giovannidf96@gmail.com>
2021-11-20 11:06:28 +0100

    Edit Farmer product

--- client/src/Components/FarmerHome/FarmerHome.js
+++ client/src/Components/FarmerHome/FarmerHome.js
@@ -10,10 +10,17 @@ import ima from '../ProductImages/p1-1.jpg'
 function FarmerHome() {
 
     const [show, setShow] = useState(false);
+    const [edit, setEdit] = useState(false);
+    const [productToEdit, setProductToEdit] = useState();
 
     const handleClose = () => {
         setShow(false);
     }
+
+    const handleCloseEdit = () => {
+        setEdit(false);
+    }
+
 
 
     return (
@@ -28,13 +35,34 @@ function FarmerHome() {
 
                 </Modal.Body>
             </Modal>
+
+
+            <Modal show={edit} onHide={handleCloseEdit} centered>
+                <Modal.Header closeButton>
+                    <Modal.Title>Edit product</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <EditProductForm productToEdit={productToEdit}/>
+
+                </Modal.Body>
+            </Modal>
+
+
             <ListGroup className="prodL">
-                {['Apple', 'Cheese', 'Carrots'].map((product) => (
+                {['Spaghetti', 'Cheese', 'Carrots','Apple'].map((product) => (
+
+
                 <ListGroup.Item>
                     <Row>
-                        <Col md="2"><Image className="ima" src={ima} rounded /></Col>
+                        <Col md="2">
+                            <img src={`https://source.unsplash.com/200x200/?${product} food`} />
+                        </Col>
                         <Col md="8" ><h4 className="textP">{product}</h4></Col>
-                        <Col md="2"><Button  className="buttonEdit" variant="secondary" onClick={() => setShow(true)}>
+                        <Col md="2"><Button  className="buttonEdit" variant="secondary" onClick={() => {
+                            setEdit(true);
+                            setProductToEdit(product);
+                        }
+                        }>
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                                  className="bi bi-pencil" viewBox="0 0 16 16">
                                 <path
@@ -43,7 +71,8 @@ function FarmerHome() {
 
                     </Row>
                     </ListGroup.Item>
-                ))}
+                )
+                )}
             </ListGroup>
     <Button className="addP" variant="primary" onClick={() => setShow(true)}>Add product</Button>
         </>
@@ -118,6 +147,122 @@ function AddProductForm(props) {
                     <Form.Group controlId="formName">
                         <Form.Label>Name</Form.Label>
                         <Form.Control required type='text' value={name} onChange={ev => setName(ev.target.value)} />
+                    </Form.Group>
+                    <Form.Group controlId="formDescription">
+                        <Form.Label>Description</Form.Label>
+                        <Form.Control required type='text' value={description} onChange={ev => setDescription(ev.target.value)} />
+                    </Form.Group>
+                    <Form.Group controlId="formQuantity">
+                        <Form.Label>Quantity</Form.Label>
+                        <Form.Control required type='text' value={quantity} onChange={ev => setQuantity(ev.target.value)} />
+                    </Form.Group>
+                    <Form.Group controlId="formType">
+                        <Form.Label>Type</Form.Label>
+
+                        <Form.Select aria-label="Default select example">
+                            <option>Select product type</option>
+                            <option value={type} onSelect={ev =>{  setType(ev.target.value);
+                                setTypeName('Fruits and Vegetables')
+                            }}>Fruits and Vegetables</option>
+                            <option value={type} onSelect={ev =>{  setType(ev.target.value);
+                                setTypeName('Dairy')
+                            }}>Dairy</option>
+                            <option value={type} onSelect={ev =>{  setType(ev.target.value);
+                                setTypeName('Meat and salumi')
+                            }}>Meat and salumi</option>
+                            <option value={type} onSelect={ev =>{  setType(ev.target.value);
+                                setTypeName('Sea products')
+                            }}>Sea products</option>
+                            <option value={type} onSelect={ev =>{  setType(ev.target.value);
+                                setTypeName('Baker and sweets')
+                            }}>Baker and sweets</option>
+                            <option value={type} onSelect={ev =>{  setType(ev.target.value);
+                                setTypeName('Beverages')
+                            }}>Beverages</option>
+                        </Form.Select>
+
+                    </Form.Group>
+                    <Form.Group className="mt-2" controlId="formPricePerUnit">
+                        <Form.Label>Price per unit</Form.Label>
+                        <Form.Control required type='text' value={pricePerUnit} onChange={ev => setPricePerUnit(ev.target.value)} />
+                    </Form.Group>
+
+
+                    <Button className="mt-3" onClick={handleAdd}>Register</Button>
+                    <Form.Text className="text-danger">{errorMessage}</Form.Text>
+                </Form >
+            </>
+        );
+    }
+
+}
+
+function EditProductForm(props) {
+    const [name, setName] = useState('');
+    const [description, setDescription] = useState('');
+    const [quantity, setQuantity] = useState('');
+    const [type, setType] = useState('');
+    const [pricePerUnit, setPricePerUnit] = useState('');
+
+    const [errorMessage, setErrorMessage] = useState('');
+    const [submitted, setSubmitted] = useState(false);
+    const [typeName, setTypeName] = useState('Select product type');
+
+    const handleAdd = (event) => {
+        event.preventDefault();
+
+        let valid = true;
+        if (name === '') {
+            setErrorMessage('Missing name description!');
+            valid = false;
+        }
+        if (description === '') {
+            setErrorMessage('Missing surname description!');
+            valid = false;
+        }
+        if (quantity === '') {
+            setErrorMessage('Missing password description!');
+            valid = false;
+        }
+        if (type === '') {
+            setErrorMessage('Missing email description!');
+            valid = false;
+        }
+        if (pricePerUnit === '') {
+            setErrorMessage('Missing phoneNumber description!');
+            valid = false;
+        }
+
+
+
+        if (valid) {
+            //props.addClient(new Client(name, surname, email, password, phoneNumber, address));
+            formReset();
+            setSubmitted(true);
+        }
+    };
+
+    const formReset = () => {
+        setName('')
+        setDescription('');
+        setQuantity('');
+        setType('');
+        setPricePerUnit('');
+
+
+    };
+
+    if (submitted) {
+        return (
+            <Redirect to='/' />
+        );
+    } else {
+        return (<>
+                <Form>
+                    <img src={`https://source.unsplash.com/200x200/?${props.productToEdit} food`} />
+                    <Form.Group controlId="formName">
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control required type='text' defaultValue={props.productToEdit} onChange={ev => setName(ev.target.value)} />
                     </Form.Group>
                     <Form.Group controlId="formDescription">
                         <Form.Label>Description</Form.Label>
