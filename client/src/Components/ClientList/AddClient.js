9bba2a642a37359ae49b89ffca01c1afb89e392c
MLetiziaColangelo <m.letiziacolangelo@gmail.com>
2021-11-22 18:05:35 +0100

    Registration success feedback

--- client/src/Components/ClientList/AddClient.js
+++ client/src/Components/ClientList/AddClient.js
@@ -130,7 +130,11 @@ function AddClientForm(props) {
 
     if (submitted) {
         return (<>
-            <Redirect to='/' />
+           <Redirect to={{
+            pathname: '/',
+            state: { showToast: true }
+        }}
+/>
         </>
         );
     } else {
