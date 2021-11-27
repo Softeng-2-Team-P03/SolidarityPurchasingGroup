57e2f642c5dee94c8d3882fa23f5061af30cde54
Luca Bianconi s287871 <s287871@studenti.polito.it>
2021-11-27 14:30:00 +0100

    navbar css sonar fixes

--- client/src/Components/HomePage/HomePage.js
+++ client/src/Components/HomePage/HomePage.js
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './HomePage.css';
 import { Link, useLocation } from 'react-router-dom';
-import { Col, Row, Button, Dropdown , Toast} from "react-bootstrap"; 
+import { Col, Row, Button, Toast} from "react-bootstrap"; 
 import { useState } from "react";
 
 function HomePage(prop) {
