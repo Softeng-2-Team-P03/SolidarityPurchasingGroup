bd4774c07687d1a2b0b887d722cb3df33b3c4251
Luca Bianconi s287871 <s287871@studenti.polito.it>
2021-11-27 15:02:29 +0100

    small smells fixes

--- client/src/api/product-api.js
+++ client/src/api/product-api.js
@@ -2,7 +2,6 @@
  * All the API For Product
  */
 import React from "react";
-//import { propTypes } from "react-bootstrap/esm/Image";
 const BASEURL = '/api';
 async function getAllProducts(arg) {
     // call: GET /api/products
