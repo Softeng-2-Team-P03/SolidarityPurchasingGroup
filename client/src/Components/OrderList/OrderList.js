52578995a41083fd463bf0f4272046bfd386fe4b
Paolobd <paolobodnarescul@gmail.com>
2021-11-28 22:50:56 +0100

    Added some tests on orderlist

--- client/src/Components/OrderList/OrderList.js
+++ client/src/Components/OrderList/OrderList.js
@@ -2,7 +2,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './OrderList.css';
 import bookingApi from '../../api/booking-api';
-import { Button, Form, Table } from "react-bootstrap";
+import { Spinner, Form, Table } from "react-bootstrap";
 import React, { useEffect, useState } from 'react';
 //import FormCheckLabel from 'react-bootstrap/esm/FormCheckLabel';
 
@@ -16,8 +16,7 @@ function Order(props) {
         let time = new Date(localStorage.getItem('virtualDate'));
         const day = time.getDay();
         const hour = time.getHours();
-        console.log("day "+day +" hour "+hour)
-        if (day >= 3 && hour >= 8 && hour<=19 && day<= 5 ) 
+        if (day >= 3 && hour >= 8 && hour <= 19 && day <= 5)
             setTimeEnabled(true);
         else
             setTimeEnabled(false);
@@ -28,7 +27,7 @@ function Order(props) {
         checkDate();
         return () => clearInterval(id);
     }, [])
-    
+
     return (
         <>
             <tr>
@@ -38,19 +37,17 @@ function Order(props) {
                 <td>{props.order.State}</td>
                 {props.order.PickupTime ? <td> {props.order.PickupTime} </td> : <td> {props.order.DeliveryTime} </td>}
                 <td>{props.order.TotalPrice}</td>
-                
-                {console.log(timeEnabled)}
                 {timeEnabled ? <td ><Form.Check
                     type="switch"
                     id="custom-switch"
                     label={isHandedOut}
                     onClick={() => { isHandedOut === "click to hand it out" ? setIsHandedOut("Handed out") : setIsHandedOut("click to hand it out") }}
                 /></td> : <td ><Form.Check
-                type="switch"
-                id="custom-switch"
-                label={isHandedOut}
-                disabled
-            /></td>}
+                    type="switch"
+                    id="custom-switch"
+                    label={isHandedOut}
+                    disabled
+                /></td>}
             </tr>
         </>
     );
@@ -63,16 +60,17 @@ function OrderList(props) {
     const [orders, setOrders] = useState([]);
     const [searchOrders, setSearchOrders] = useState([]);
 
-    const [loadingProducts, setLoadingProducts] = useState(true);
+    const [loadingOrders, setLoadingOrders] = useState(true);
     const [errorLoading, setErrorLoading] = useState(''); //Error in loading orders
 
 
 
     useEffect(() => {
         bookingApi.getOrders().then((orders) => {
-            setOrders(orders.map(order => ({ ...order})));
-            setSearchOrders(orders.map(order => ({ ...order})));
-            setLoadingProducts(false);
+            setOrders(orders.map(order => ({ ...order })));
+            setSearchOrders(orders.map(order => ({ ...order })));
+            setLoadingOrders(false);
+            setErrorLoading('');
         }).catch(err => {
             setErrorLoading('Error during the loading of the orders')
             console.error(err);
@@ -92,30 +90,33 @@ function OrderList(props) {
     return (
         <>
             <Form.Control type="text" className="searchB" placeholder="Search order" onChange={x => changeSearchText(x.target.value)} />
-            <Table responsive striped bordered hover className="ordersTable">
-                <thead>
-                    <tr>
-                        <th>OrderID</th>
-                        <th>UserId</th>
-                        <th>Booking Issue Date </th>
-                        <th>State</th>
-                        <th>Scheduled Date</th>
-                        <th>Total price</th>
-                        <th>Handed out</th>
-
-                        <th width="13%"></th>
-                    </tr>
-                </thead>
-                <tbody> {
-                    searchOrders.map((or) =>
-                        <Order key={or.BookingID}
-                            order={or}
-                        />)
-                }
-                </tbody>
-            </Table>
+            {loadingOrders ? <h1 style={{ textAlign: "center" }}>Loading orders... <Spinner animation="border" /></h1>
+                :
+                <Table data-testid="tableOrders" responsive striped bordered hover className="ordersTable">
+                    <thead>
+                        <tr>
+                            <th>OrderID</th>
+                            <th>UserId</th>
+                            <th>Booking Issue Date </th>
+                            <th>State</th>
+                            <th>Scheduled Date</th>
+                            <th>Total price</th>
+                            <th>Handed out</th>
+                            <th width="13%"></th>
+                        </tr>
+                    </thead>
+                    <tbody> {
+                        searchOrders.map((or) =>
+                            <Order key={or.BookingID}
+                                order={or}
+                            />)
+                    }
+                    </tbody>
+                </Table>
+            }
+            <h2 style={{ textAlign: "center", color: "red" }}>{errorLoading}</h2>
         </>
     );
 }
 
-export default OrderList;
+export {OrderList, Order};
