c90ec040d3a5fb900ec23f784d2f84a39c50872f
Giovanni De Florio s292457@studenti.polito.it <giovannidf96@gmail.com>
2021-12-09 21:33:50 +0100

    Notification bell

--- client/src/Components/Notifications/Notifications.js
+++ client/src/Components/Notifications/Notifications.js
@@ -1,20 +1,65 @@
 import NavBar from "../NavBar/NavBar";
 import {useState} from "react";
-import {Button, Offcanvas} from "react-bootstrap";
+import {Offcanvas} from "react-bootstrap";
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import './Notifications.css'
+import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
+import {Badge, IconButton} from "@mui/material";
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import {yellow} from "@mui/material/colors";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            light: '#ff7961',
+            main: '#ff0000',
+            dark: '#ba000d',
+            contrastText: '#fff',
+        },
+    },
+});
 
 function Notifications({ name, ...props }) {
+
     const [show, setShow] = useState(false);
+    const [countNoti, setCountNoti] = useState(100);
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
+    function notificationsLabel(count) {
+        if (count === 0) {
+            return 'no notifications';
+        }
+        if (count > 99) {
+            return 'more than 99 notifications';
+        }
+        return `${count} notifications`;
+    }
+
     return (
         <>
-            <i variant="primary" onClick={handleShow} class="bi bi-bell-fill text-warning" style={{ fontSize: 25 }}>
-                {name}
-            </i>
+            {countNoti!==0 ?
+                <ThemeProvider theme={theme}>
+                    <IconButton aria-label={notificationsLabel(countNoti)} onClick={handleShow}>
+                        <Badge badgeContent={countNoti} color="primary">
+                            <NotificationsActiveIcon fontSize={"large"} sx={{color: yellow[500]}}/>
+                        </Badge>
+                    </IconButton>
+                </ThemeProvider>
+                :
+                <ThemeProvider theme={theme}>
+                    <IconButton onClick={handleShow}>
+                        <Badge color="primary">
+                            <NotificationsIcon fontSize={"large"} sx={{color: yellow[500]}}/>
+                        </Badge>
+                    </IconButton>
+                </ThemeProvider>
+
+            }
+
+
             <Offcanvas show={show} onHide={handleClose} {...props}>
                 <Offcanvas.Header closeButton>
                     <Offcanvas.Title>Offcanvas</Offcanvas.Title>
