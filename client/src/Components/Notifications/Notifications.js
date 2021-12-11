04f754ca14c7c303e392917c5e753766544910ea
Paolobd <paolobodnarescul@gmail.com>
2021-12-11 19:46:45 +0100

    Added dismiss notification functionality

--- client/src/Components/Notifications/Notifications.js
+++ client/src/Components/Notifications/Notifications.js
@@ -8,6 +8,7 @@ import { Badge, IconButton } from "@mui/material";
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import userApi from '../../api/user-api';
+import api from '../../API';
 
 const theme = createTheme({
     palette: {
@@ -25,20 +26,25 @@ function Notifications({ name, ...props }) {
     const [show, setShow] = useState(false);
     const [countNoti, setCountNoti] = useState(0);
     const [notifications, setNotifications] = useState("");
+    const [dirty, setDirty] = useState(true);
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
     useEffect(() => {
 
-        userApi.getNotifications().then(notifications => {
-            setNotifications(notifications);
-            setCountNoti(notifications.length);
-        })
-            .catch(err => {
-                console.error(err);
-            });
-    }, []);
+        if (dirty) {
+            userApi.getNotifications().then(notifications => {
+                setNotifications(notifications);
+                setCountNoti(notifications.length);
+                setDirty(false);
+            })
+                .catch(err => {
+                    console.error(err);
+                });
+        }
+
+    }, [dirty]);
 
 
     function notificationsLabel(count) {
@@ -49,6 +55,20 @@ function Notifications({ name, ...props }) {
             return 'more than 99 notifications';
         }
         return `${count} notifications`;
+    }
+
+    const dismissNotification = (id) => {
+        api.updateNotificationVisibility(1, id)
+            .then(() => setDirty(true))
+            .catch(err => console.error(err));
+    }
+
+    const dismissAll = () => {
+        notifications.forEach((notification) => {
+            api.updateNotificationVisibility(1, notification.notificationId)
+            .catch(err => console.error(err));
+        })
+        setDirty(true);
     }
 
     return (
@@ -76,27 +96,30 @@ function Notifications({ name, ...props }) {
             <Offcanvas className="offC" show={show} onHide={handleClose} {...props}>
                 <Offcanvas.Header closeButton>
                     <Offcanvas.Title>Notification</Offcanvas.Title>
+                    <button type="button" className="btn btn-warning" disabled={countNoti === 0}
+                        onClick={() => dismissAll()}>Dismiss All</button>
                 </Offcanvas.Header>
-                
+
                 {countNoti !== 0 ?
                     <Offcanvas.Body>
-                    {notifications.map((notification) =>
-                        
+                        {notifications.map((notification) =>
+
                             <Alert variant="warning">
                                 <Alert.Heading>{notification.notificationHeader}</Alert.Heading>
                                 <p>
-                                {notification.notificationBody}
+                                    {notification.notificationBody}
                                 </p>
                                 <hr />
                                 <div className="d-flex justify-content-end">
-                                    <button type="button" className="btn btn-warning">Dismiss</button>
+                                    <button type="button" className="btn btn-warning"
+                                        onClick={() => dismissNotification(notification.notificationId)}>Dismiss</button>
                                 </div>
                             </Alert>
-                        
 
-                    )}
+
+                        )}
                     </Offcanvas.Body>
-                     
+
                     :
                     <Offcanvas.Body className="noNoty">
                         You don't have notifications to be shown
