e3ed09dbbca7076700dfbe05885b366fd4e55840
MLetiziaColangelo <62267634+MLetiziaColangelo@users.noreply.github.com>
2022-01-11 09:33:16 +0100

    Fixed e2e story3

--- client/cypress/integration/story3.spec.js
+++ client/cypress/integration/story3.spec.js
@@ -9,9 +9,14 @@
 
 describe('Browse products in shop', () => {
     it('successfully loads', () => {
+        cy.viewport(1920, 1080)
         cy.visit('http://localhost:3000') // change URL to match your dev URL
-        cy.get(':nth-child(2) > a > .btn').click({force: true})
         cy.get('#resetTime').click()
+        cy.get('#day').type('{selectall}28').should('have.value', '28')
+        cy.get('#month').type('{selectall}11').should('have.value', '11')
+        cy.get('#year').type('{selectall}2021' ).should('have.value', '2021') 
+        cy.get(':nth-child(2) > a > .btn').click({force: true})
+     
 
     })
 })
@@ -19,18 +24,23 @@ describe('Browse products in shop', () => {
 
 describe('Browse products in shop by logged user', () => {
     it('successfully loads', () => {
+        cy.viewport(1920, 1080)
         cy.visit('http://localhost:3000') // change URL to match your dev URL
+        cy.get('#resetTime').click()
         cy.contains('Login').click()
         cy.get('#formBasicEmail')
-            .type('federico@spg.com')
-            .should('have.value', 'federico@spg.com')
+            .type('federico.clientespg@gmail.com')
+            .should('have.value', 'federico.clientespg@gmail.com')
         cy.get('#formBasicPassword')
             .type('26gKpQK9')
             .should('have.value', '26gKpQK9')
         cy.get('.btn').click()
         cy.get('[href="/"]').click()
-        cy.get('#basic-navbar-nav > :nth-child(2) > [href="/products"]').click()
-        cy.get('#resetTime').click()
+        cy.get('#day').type('{selectall}28').should('have.value', '28')
+        cy.get('#month').type('{selectall}11').should('have.value', '11')
+        cy.get('#year').type('{selectall}2021' ).should('have.value', '2021')
+        cy.get(':nth-child(1) > [href="/products"]').click()
+       
 
     })
 })
